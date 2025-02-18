a2b974acfd7288ee7e1d6401c76ad46c0226dd57
mohan <jagadabimohan@gmail.com>
2025-02-18 16:03:07 +0530

    This is commit form AST

---
+++ src/Automation/Rest.java
@@ -0,0 +1,11 @@
+package Automation;
+
+public class Rest {
+	 public void postJira()
+	 {
+		System.out.println("postJira");
+		System.out.println("postJira2");
+		System.out.println("postJira3");
+	 }
+
+}
